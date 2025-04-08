28dd429ce8b4e5411b37491ddfedce024c6b53fb
JustAMob <carloencillo@gmail.com>
2025-04-08 20:29:58 +0800

    added database connection

--- src/main/java/com/cjme/motorphsystem/util/DBConnection.java
+++ src/main/java/com/cjme/motorphsystem/util/DBConnection.java
@@ -4,10 +4,20 @@
  */
 package com.cjme.motorphsystem.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author JustAMob
  */
 public class DBConnection {
-    
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/motorphdatabase";
+        String user = "root";
+        String password = "01Tartaros";
+        return DriverManager.getConnection(url, user, password);
+    }
 }
+
