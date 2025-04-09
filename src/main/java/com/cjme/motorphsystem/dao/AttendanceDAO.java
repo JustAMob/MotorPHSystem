a2315ec71de6f664552ec0de28d8b6acd98a512e
JustAMob <carloencillo@gmail.com>
2025-04-09 14:00:43 +0800

    addedd the timelog table and ways to access the timelog database

--- src/main/java/com/cjme/motorphsystem/dao/AttendanceDAO.java
+++ src/main/java/com/cjme/motorphsystem/dao/AttendanceDAO.java
@@ -1,13 +1,145 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.cjme.motorphsystem.dao;
+
+import com.cjme.motorphsystem.model.Attendance;
+import com.cjme.motorphsystem.util.DBConnection;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author JustAMob
  */
 public class AttendanceDAO {
+    private final Connection connection;
+
+    public AttendanceDAO() throws SQLException {
+        this.connection = DBConnection.getConnection();
+    }
     
+    //CREATE
+    public boolean addTimelog (Attendance attendance) throws SQLException {
+        String sql = "INSERT INTO timelog (employee_id, log_date, time_in, time_out) VALUES(?,?,?,?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1,attendance.getEmployeeID());
+            stmt.setDate(2,attendance.getLogDate());
+            stmt.setTime(3,attendance.getTimeIn());
+            stmt.setTime(4,attendance.getTimeOut());
+            return stmt.executeUpdate()>0;
+        }
+    }
+    
+    //Gets all timelogs for a specific employee.
+    public List<Attendance> getTimeLogsByEmployee(int employeeID) throws SQLException {
+        List<Attendance> list = new ArrayList<>();
+        String sql = "SELECT * FROM timelog WHERE employee_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Attendance attendance = new Attendance (
+                        rs.getInt("timelog_id"),
+                        rs.getInt("employee_id"),
+                        rs.getDate("log_date"),
+                        rs.getTime("time_in"),
+                        rs.getTime("time_out")
+                    );
+                list.add(attendance);
+            }
+        }
+        return list;
+    }
+    
+    //Gets timelogs of an employee for a specific month and year
+    public List<Attendance> getTimeLogsByEmployeeAndMonth(int employeeID, int year, int month) throws SQLException {
+        List<Attendance> list = new ArrayList<>();
+        String sql = "SELECT * FROM timelog WHERE employee_id = ? AND YEAR(log_date) = ? AND MONTH(log_date) = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            stmt.setInt(2, year);
+            stmt.setInt(3, month);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Attendance attendance = new Attendance (
+                        rs.getInt("timelog_id"),
+                        rs.getInt("employee_id"),
+                        rs.getDate("log_date"),
+                        rs.getTime("time_in"),
+                        rs.getTime("time_out")
+                    );
+                list.add(attendance);
+            }
+        }
+        return list;
+    }
+  
+    //Gets all timelogs for a specific date    
+    public List<Attendance> getTimeLogsByDate(Date date) throws SQLException {
+        List<Attendance> list = new ArrayList<>();
+        String sql = "SELECT * FROM timelog WHERE log_date = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Attendance attendance = new Attendance (
+                        rs.getInt("timelog_id"),
+                        rs.getInt("employee_id"),
+                        rs.getDate("log_date"),
+                        rs.getTime("time_in"),
+                        rs.getTime("time_out")
+                    );
+                list.add(attendance);
+            }
+        }
+        return list;
+    }
+    
+    //Gets all timelogs within a date range
+    public List<Attendance> getTimeLogsByRange(Date startDate, Date endDate) throws SQLException {
+        List<Attendance> list = new ArrayList<>();
+            String sql = "SELECT * FROM timelog WHERE log_date BETWEEN ? AND ?";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.setDate(1, startDate);
+                stmt.setDate(2, endDate);
+
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    Attendance attendance = new Attendance (
+                            rs.getInt("timelog_id"),
+                            rs.getInt("employee_id"),
+                            rs.getDate("log_date"),
+                            rs.getTime("time_in"),
+                            rs.getTime("time_out")
+                        );
+                    list.add(attendance);
+                }
+            }
+            return list;
+    }
+        
+    //Gets all timelogs for an employee on a specific day
+    public List<Attendance> getTimeLogsByEmployeeAndDate (int employeeID, Date date) throws SQLException {
+        List<Attendance> list = new ArrayList<>();
+        String sql = "SELECT * FROM timelog WHERE employee_id = ? AND DATE(log_date) = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            stmt.setDate(2, date);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Attendance attendance = new Attendance (
+                        rs.getInt("timelog_id"),
+                        rs.getInt("employee_id"),
+                        rs.getDate("log_date"),
+                        rs.getTime("time_in"),
+                        rs.getTime("time_out")
+                    );
+                list.add(attendance);
+            }
+        }
+        return list;
+   }
 }
--- src/main/java/com/cjme/motorphsystem/model/Attendance.java
+++ src/main/java/com/cjme/motorphsystem/model/Attendance.java
@@ -4,10 +4,80 @@
  */
 package com.cjme.motorphsystem.model;
 
+import java.sql.Date;
+import java.sql.Time;
+
 /**
  *
  * @author JustAMob
  */
 public class Attendance {
+    private int logID;
+    private int employeeID;
+    private Date logDate;
+    private Time timeIn;
+    private Time timeOut;
+
+    public Attendance(int logID, int employeeID, Date logDate, Time timeIn, Time timeOut) {
+        this.logID = logID;
+        this.employeeID = employeeID;
+        this.logDate = logDate;
+        this.timeIn = timeIn;
+        this.timeOut = timeOut;
+    }
+
+    public Attendance(int employeeID, Date logDate, Time timeIn, Time timeOut) {
+        this.employeeID = employeeID;
+        this.logDate = logDate;
+        this.timeIn = timeIn;
+        this.timeOut = timeOut;
+    }
+
+    public int getLogID() {
+        return logID;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public Date getLogDate() {
+        return logDate;
+    }
+
+    public Time getTimeIn() {
+        return timeIn;
+    }
+
+    public Time getTimeOut() {
+        return timeOut;
+    }
+
+    public void setLogDate(Date logDate) {
+        this.logDate = logDate;
+    }
+
+    public void setTimeIn(Time timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public void setTimeOut(Time timeOut) {
+        this.timeOut = timeOut;
+    }
+    
+    @Override
+    public String toString(){
+        return "Attendance:{"+
+                "timelogID" + logID +
+                "employeeID" + employeeID +
+                "logdate" + logDate +
+                "timeIn" + timeIn +
+                "timeOut" + timeOut +
+                '}';
+    }
+    
+    
+    
+    
     
 }
