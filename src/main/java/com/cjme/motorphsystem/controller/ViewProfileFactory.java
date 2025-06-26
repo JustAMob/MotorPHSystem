01acf7f47e92c7b41cb2f6f11cb48ced65f60212
Gyabu <lr.jgportillo@mmdc.mcl.edu.ph>
2025-06-26 16:18:42 +0800

    removedErrors

---
+++ src/main/java/com/cjme/motorphsystem/controller/ViewProfileFactory.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cjme.motorphsystem.controller;
+
+
+/**
+ * ViewProfileFactory is responsible for creating appropriate profile view
+ * frames based on the user's role and permissions.
+ *
+ * @author Lance
+ */
+public class ViewProfileFactory {
+
+    private static EmployeeSearchPage employeeSearchPage;
+
+    /**
+     * Creates a profile view frame based on user role and permissions.
+     *
+     * @param role The role of the user.
+     * @param employeeInformation The employee details to be displayed.
+     * @return An appropriate ViewProfileFrame based on the user's permissions.
+     * @throws com.mycompany.motorph.manager.RBACManager.InvalidRoleException
+     */
+}
