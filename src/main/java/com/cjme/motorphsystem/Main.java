414a8c9a3d6ff6230370bd093d1faa4e6e9aad2c
MYSTRi-DECiPHER <devcphr@gmail.com>
2025-07-10 10:12:24 +0800

    Updated GUI to FlatLaf, Fixed GUI Issues

--- src/main/java/com/cjme/motorphsystem/Main.java
+++ src/main/java/com/cjme/motorphsystem/Main.java
@@ -2,6 +2,8 @@ package com.cjme.motorphsystem;
 
 
 import com.cjme.motorphsystem.controller.Login;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import javax.swing.UIManager;
 
 /**
  *
@@ -11,12 +13,26 @@ import com.cjme.motorphsystem.controller.Login;
 
 public class Main {
     public static void main(String[] args) {
+        
+        try {
+            FlatMacDarkLaf.setup(); 
+        } catch (Exception e) {
+            
+            System.err.println("Failed to initialize FlatLaf Cupertino Dark: " + e);
+           
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ex) {
+                
+            }
+        }
        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 Login login = new Login();
                 login.setVisible(true);
             }
+            
         });
     }
 }
